a46441f52ddde5a324b22f62a6551f816b2df681
ld190 <ld190@duke.edu>
2018-09-03 10:15:36 -0400

    ls

--- 012_complex_ty/mytypes.c
+++ 012_complex_ty/mytypes.c
@@ -1,9 +1,9 @@
-(1) struct complex_num_tag {
+struct complex_num_tag {
   double real_part;
   double imaginary_part;
 };
 typedef struct complex_num_tag complex_num_t;
-(2) enum animal_tag {
+enum animal_tag {
   CAT,
   FISH,
   SNAKE,

bccb7777146da6ba1d02e5639b54a33616a680b0
ld190 <ld190@duke.edu>
2018-09-06 23:39:50 -0400

    ls

--- 020_rect/rectangle.c
+++ 020_rect/rectangle.c
@@ -17,13 +17,81 @@ int max(int a, int b) {
 }
 
 // Declare your rectangle structure here!
+struct rectangles {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rectangles rectangle_t;
 
 rectangle_t canonicalize(rectangle_t r) {
-  // WRITE THIS FUNCTION
+  if (r.height <= 0) {
+    r.height = -r.height;
+    r.y = r.y - r.height;
+  }
+  else {
+    r.height = r.height;
+  }
+  if (r.width <= 0) {
+    r.x = r.x + r.width;
+    r.width = -r.width;
+  }
+  else {
+    r.width = r.width;
+  }
   return r;
 }
 rectangle_t intersection(rectangle_t r1, rectangle_t r2) {
-  // WRITE THIS FUNCTION
+  rectangle_t r_left;
+  rectangle_t r_right;
+  rectangle_t r_low;
+  rectangle_t r_high;
+
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  int x = max(r1.x, r2.x);
+  int y = max(r1.y, r2.y);
+  if (x == r1.x) {
+    r_left = r2;
+    r_right = r1;
+  }
+  else {
+    r_left = r1;
+    r_right = r2;
+  }
+  if (y == r1.y) {
+    r_high = r1;
+    r_low = r2;
+  }
+  else {
+    r_high = r2;
+    r_low = r1;
+  }
+
+  r1.x = x;
+  r1.y = y;
+
+  if ((r_right.x >= r_left.x) && ((r_right.x + r_right.width) < (r_left.x + r_left.width))) {
+    r1.width = min(r1.width, r2.width);
+  }
+  else if (r_left.x + r_left.width < r_right.x) {
+    r1.width = r1.height = 0;
+  }
+  else {
+    r1.width = r_left.x + r_left.width - r_right.x;
+  }
+
+  if ((r_high.y >= r_low.y) && ((r_low.y + r_low.height) >= (r_high.y + r_high.height))) {
+    r1.height = min(r1.height, r2.height);
+  }
+  else if (r_low.y + r_low.height < r_high.y) {
+    r1.height = r1.width = 0;
+  }
+  else {
+    r1.height = r_low.y + r_low.height - r_high.y;
+  }
+
   return r1;
 }
 
@@ -32,7 +100,8 @@ void printRectangle(rectangle_t r) {
   r = canonicalize(r);
   if (r.width == 0 && r.height == 0) {
     printf("<empty>\n");
-  } else {
+  }
+  else {
     printf("(%d,%d) to (%d,%d)\n", r.x, r.y, r.x + r.width, r.y + r.height);
   }
 }

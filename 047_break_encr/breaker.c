5a95e30659b730cbef0788f758a4e140c83c877b
ld190 <ld190@duke.edu>
2018-09-25 15:07:08 -0400

    789789

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -1,36 +1,42 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-int sort(int * array) {
-  int max = 0;
-  for (int i = 0; i < 26; i++) {
-    if (max < array[i]) {
-      max = i;
+int findlargestIndex(int * array, int n) {
+  if (n <= 0) {
+    return -1;
+  }
+  int largestIndex = 0;
+  for (int i = 1; i < n; i++) {
+    if (array[i] > array[largestIndex]) {
+      largestIndex = i;
     }
   }
-  return max;
+  return largestIndex;
 }
 char MostFrequent(FILE * f) {
-  int alphabet[27];
+  int alphabet[26] = {0};
   int c;
   while ((c = fgetc(f)) != EOF) {
     if (isalpha(c)) {
       c = tolower(c);
-      alphabet[c - 97]++;
+      alphabet[c - 'a']++;
     }
   }
-  int number = sort(alphabet);
-  return ('a' + number);
+  int number = findlargestIndex(alphabet, 26);
+  return number;
 }
 int main(int argc, char ** argv) {
-  if (argc != 3) {
+  if (argc != 2) {
     fprintf(stderr, "Usage: encrypt key inputFileName\n");
     return EXIT_FAILURE;
   }
-  int key;
-  FILE * f = fopen(argv[2], "r");
-  key = MostFrequent(f) - 'e';
-  printf("%d", key);
+
+  FILE * f = fopen(argv[1], "r");
+  int ans = MostFrequent(f) - 4;
+  if (ans < 0) {
+    ans = ans + 26;
+  }
+  printf("%d\n", ans);
   if (fclose(f) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;

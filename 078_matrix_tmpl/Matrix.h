133d4d274c5372b2a6be7588991e752806e8081e
ld190 <ld190@duke.edu>
2018-10-21 23:14:10 -0400

    nbui

--- 078_matrix_tmpl/Matrix.h
+++ 078_matrix_tmpl/Matrix.h
@@ -18,6 +18,8 @@ class Matrix
  public:
   Matrix() : numRows(0), numColumns(0) { rows.clear(); }
   Matrix(int r, int c) : numRows(r), numColumns(c) {
+    if (r < 0)
+      numRows = 0;
     rows.resize(r);  //
     for (int i = 0; i < numRows; i++) {
       rows[i].resize(c);  //
@@ -60,11 +62,11 @@ class Matrix
   int getRows() const { return numRows; }
   int getColumns() const { return numColumns; }
   const std::vector<T> & operator[](int index) const {
-    assert(index < numRows);
+    assert(index < numRows && index >= 0);
     return rows[index];
   }
   std::vector<T> & operator[](int index) {
-    assert(index < numRows);
+    assert(index < numRows && index >= 0);
     return rows[index];
   }
 

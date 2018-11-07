9aa6275c378468ae839a2f33c001b0eba7708c4f
ld190 <ld190@duke.edu>
2018-11-07 18:30:23 -0500

    Sdsd

--- 096_bst_set/set.h
+++ 096_bst_set/set.h
@@ -3,11 +3,12 @@
 #include <stdexcept>
 
 template<typename T>
-class Set {
-public:
+class Set
+{
+ public:
   virtual void add(const T & key) = 0;
-  virtual bool contains(const T& key) const = 0;
-  virtual void remove(const T& key) = 0;
+  virtual bool contains(const T & key) const = 0;
+  virtual void remove(const T & key) = 0;
   virtual ~Set<T>() {}
 };
 #endif

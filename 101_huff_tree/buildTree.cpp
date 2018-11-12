253ede2376f48d14fa1557cefa21bbea164ee480
ld190 <ld190@duke.edu>
2018-11-12 11:58:16 -0500

    sdsd

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,22 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  Node * top1 = NULL;
+  Node * top2 = NULL;
+  while (pq.size() > 1) {
+    top1 = pq.top();
+    pq.pop();
+    top2 = pq.top();
+    pq.pop();
+    pq.push(new Node(top1, top2));
+  }
+  return pq.top();
+
   //WRITE ME!
-}  
+}

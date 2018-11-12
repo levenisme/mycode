d956d67f864e56a424814d8a337f99eda156a848
ld190 <ld190@duke.edu>
2018-11-12 15:48:44 -0500

    dsd

--- 103_huff_all/compress.cpp
+++ 103_huff_all/compress.cpp
@@ -69,6 +69,7 @@ int main(int argc, char ** argv) {
   std::map<unsigned, BitString> theMap;
   BitString null;
   tree->buildMap(null, theMap);
+  writeCompressedOutput(argv[1], argv[2], theMap);
   delete tree;
 
   //Implement main
